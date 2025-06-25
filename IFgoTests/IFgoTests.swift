6cef1e4aeba481488f7965d7f1fe0ba73203d232
pedrokekww <pedroveb119@gmail.com>
2025-06-25 14:19:06 -0300

    Initial Commit

---
+++ IFgoTests/IFgoTests.swift
@@ -0,0 +1,17 @@
+//
+//  IFgoTests.swift
+//  IFgoTests
+//
+//  Created by found on 25/06/25.
+//
+
+import Testing
+@testable import IFgo
+
+struct IFgoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
