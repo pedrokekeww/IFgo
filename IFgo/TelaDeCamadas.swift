70524bc27cf6ad9f78be8cd6976000187423a6bd
Cayque <cayque.ferreira11@aluno.ifce.edu.br>
2025-07-04 17:08:43 -0300

    Teste da nova branch

---
+++ IFgo/TelaDeCamadas.swift
@@ -0,0 +1,22 @@
+//
+//  sistemaDeCamadas.swift
+//  IFgo
+//
+//  Created by found on 04/07/25.
+//
+
+import SwiftUI
+
+struct TelaDeCamadas: View{
+    
+    var body: some View {
+        ZStack{
+            
+        }
+    }
+    
+}
+
+#Preview {
+    telaDeCamadas()
+}
