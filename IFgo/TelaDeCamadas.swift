6c3febbc5d608c34a4431b905ca2a19af43d5666
Cayque <cayque.ferreira11@aluno.ifce.edu.br>
2025-07-08 14:53:29 -0300

    Teste de pilha entre 2 andares

--- IFgo/TelaDeCamadas.swift
+++ IFgo/TelaDeCamadas.swift
@@ -5,18 +5,32 @@
 //  Created by found on 04/07/25.
 //
 
+struct Andar: Identifiable{
+    let id = UUID()
+    
+    init(nomeDaImagem: String){
+        self.imagem = nomeDaImagem
+    }
+    
+    var imagem: String
+}
+
 import SwiftUI
 
 struct TelaDeCamadas: View{
+    var andares: [Andar] = [Andar(nomeDaImagem: "2"), Andar(nomeDaImagem: "1")]
+    @State var imagemAtual: String
     
     var body: some View {
         ZStack{
-            
+            ForEach(andares){ andar in
+                Image(andar.imagem)
+            }
         }
     }
     
 }
 
 #Preview{
-    TelaDeCamadas()
+    TelaDeCamadas(imagemAtual: "1")
 }
