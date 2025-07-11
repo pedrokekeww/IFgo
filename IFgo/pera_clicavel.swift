333987a616622bd22b6c938045a9e5cf14b20b9e
Gustavo Fernandes <162316518+Kragdom@users.noreply.github.com>
2025-07-11 17:49:41 -0300

    Update pera_clicavel.swift

--- IFgo/pera_clicavel.swift
+++ IFgo/pera_clicavel.swift
@@ -1,5 +1,5 @@
 //
-//  pera_clicavel.swift
+//  regiaoClicavel.swift
 //  IFgo
 //
 //  Created by found on 11/07/25.
@@ -9,16 +9,19 @@ import SwiftUI
 
 struct regiaoClicavel: View{
     var body: some View{
+        // Navigation view a fim de preparar o navigation link
         NavigationView{
+            // Empilhamento da imagem do mapa com os botões que serão posicionados
             ZStack(alignment: .leading) {
                 Image("3")
                     .resizable()
                     .ignoresSafeArea()
                     .aspectRatio(contentMode: .fit)
-                    .border(.red)
+                    .border(.black)
                 NavigationLink(destination: TelaDeCamadas(andarAtual: Andar(nomeDaImagem: "1"))) {
                     Text("               ")
                 }
+            // Estilização do botão, manipulavel e modular
                 .padding()
                 .buttonStyle(.borderedProminent)
                 .rotationEffect(Angle(degrees: 270.0))
@@ -32,6 +35,5 @@ struct regiaoClicavel: View{
     }
 }
 #Preview {
-    peraClicavel()
+    regiaoClicavel()
 }
-
