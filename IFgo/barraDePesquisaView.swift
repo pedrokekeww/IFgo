dcc3a9a4ee8d853c3d98019a8190385b7c9314cd
Gustavo Fernandes <gustavofs0711@gmail.com>
2025-08-04 15:14:59 -0300

    uebrou a preview

--- IFgo/barraDePesquisaView.swift
+++ IFgo/barraDePesquisaView.swift
@@ -16,6 +16,7 @@ struct BarraDePesquisaView: View {
                     .autocapitalization(.none)
                     .disableAutocorrection(true)
                     .focused(mostrarHistorico)
+                    .tint(.white)
                     .onSubmit {
                         DispatchQueue.main.async { onSearch(searchText) }
                     }
